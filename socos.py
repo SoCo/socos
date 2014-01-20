2907d1429654a1654187fc8a20d3c7d0efbea6ff
Petter Aas <aaspet@gmail.com>
2014-01-20 19:25:51 +0100

    Moved call to print_current_track_info() out one level.

--- socos.py
+++ socos.py
@@ -149,10 +149,9 @@ def main():
         elif (cmd == 'play'):
             if len(sys.argv) > 3:
                 play_index(sonos, sys.argv[3])
-                print_current_track_info(sonos)
             else:
                 sonos.play()
-                print_current_track_info(sonos)
+            print_current_track_info(sonos)
         elif (cmd == 'pause'):
             print(sonos.pause())
         elif (cmd == 'stop'):
