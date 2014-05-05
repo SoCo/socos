94e84453a89117f85b9ffc86c5e99241d179f047
Petter Aas <aaspet@gmail.com>
2014-05-05 22:51:48 +0200

    Added module and class docstrings.

--- socos/__init__.py
+++ socos/__init__.py
@@ -1,3 +1,5 @@
+""" socos is a commandline tool for controlling Sonos speakers """
+
 from .core import process_cmd, shell
 
 __all__ = ['process_cmd', 'shell']
--- socos/exceptions.py
+++ socos/exceptions.py
@@ -1,2 +1,7 @@
+""" socos.exceptions contains all exceptions raised within socos """
+
+
 class SoCoIllegalSeekException(Exception):
+    """ illegal seek exception which is raised when attempting to play a
+    index outside of the queue """
     pass
