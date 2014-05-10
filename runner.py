4196a559beba05373aa061957e791b4d59ba852f
Petter Aas <aaspet@gmail.com>
2014-05-10 16:49:14 +0200

    Moved metadata from runner.py to socos/__init__.py

--- runner.py
+++ runner.py
@@ -2,11 +2,6 @@
 """ socos is a commandline tool for controlling Sonos speakers """
 
 
-# Will be parsed by setup.py to determine package metadata
-__author__ = 'SoCo team <python-soco @googlegroups.com>'
-__version__ = '0.1'
-__website__ = 'https://github.com/SoCo/socos'
-__license__ = 'MIT License'
 import sys
 from socos import process_cmd, shell
 
