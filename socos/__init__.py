8f6c3be61de2da047b70d065ccbc99b22ecfce74
Stefan Kögl <stefan@skoegl.net>
2014-05-26 18:29:33 +0200

    remove space from email address

--- socos/__init__.py
+++ socos/__init__.py
@@ -1,7 +1,7 @@
 """ socos is a commandline tool for controlling Sonos speakers """
 
 # Will be parsed by setup.py to determine package metadata
-__author__ = 'SoCo team <python-soco @googlegroups.com>'
+__author__ = 'SoCo team <python-soco@googlegroups.com>'
 __version__ = '0.1'
 __website__ = 'https://github.com/SoCo/socos'
 __license__ = 'MIT License'
